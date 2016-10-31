ae8d0f577126118337830b9b313313ae5adf231b
Andrei Kasak <Andrei_Kasak@epam.com>
2016-10-31 18:57:10 +0300

    Add extra class Markup, change logic

--- HTTP/HTTP/Program.cs
+++ HTTP/HTTP/Program.cs
@@ -16,37 +16,35 @@ namespace HTTP
         static void Main(string[] args)
         {
             var url = "www.google.com";
-            Task t = new Task(() => Do(url, 2, DomainLimitation.Without, new List<FileExtention> { FileExtention.JPEG }));
+            Task t = new Task(() => Do(url, 2, DomainLimitation.Without, new List<FileExtension> { FileExtension.JPG }));
             t.Start();
 
             Console.ReadKey();
         }
 
-        public static async void Do(string url, int deep, DomainLimitation domainLimitation, List<FileExtention> extentions)
+        public static async void Do(string url, int deep, DomainLimitation domainLimitation, List<FileExtension> extentions)
         {
             if (!url.StartsWith("http")) url = string.Format("https://{0}", url);
 
-            var markups = new List<string>();
+            var markups = new List<Markup>();
 
             var mainPage = await GetHtml(url);
-            markups.Add(mainPage);
+	        markups.Add(new Markup {Url = url, Html = mainPage});
             if (deep > 0)
             {
-                var markupsByLink = new List<string>();
+                var markupsByLink = new List<Markup>();
                 var level = 0;
-                do
+				while (level < deep)
                 {
                     if (markupsByLink.Any())
                     {
-                        markups.AddRange(markupsByLink);
-
-                        var buf = new List<string>();
+						var buf = new List<Markup>();
                         for (int index = 0; index < markupsByLink.Count; index++)
                         {
                             try
                             {
                                 var item = markupsByLink[index];
-                                var pages = await GetChildPages(item);
+								var pages = await GetChildPages(item.Html, markups.Select(x => x.Url));
                                 if (pages.Any())
                                 {
                                     buf.AddRange(pages);
@@ -54,29 +52,32 @@ namespace HTTP
                             }
                             catch (Exception e)
                             {
-                                Console.WriteLine(e);
+                              //  Console.WriteLine(e);
                             }
                         }
                         markupsByLink.Clear();
-                        markupsByLink.AddRange(buf);
+						markupsByLink.AddRange(buf);
+						markups.AddRange(markupsByLink);
                     }
                     else
                     {
-                        markupsByLink.AddRange(await GetChildPages(mainPage));
+						markupsByLink.AddRange(await GetChildPages(mainPage, markups.Select(x=>x.Url)));
+						markups.AddRange(markupsByLink);
                     }
                     level++;
-                } while (level < deep);
+                }
 
             }
 
             foreach (var markup in markups)
             {
                 var doc = new HtmlDocument();
-                doc.LoadHtml(markup);
+                doc.LoadHtml(markup.Html);
+				Console.WriteLine(markup.Url);
 
                 if (extentions.Any())
                 {
-                    foreach (var image in Extras(url, doc))
+					foreach (var image in Extras(url, doc, extentions))
                     {
                         Console.WriteLine(image.Url.Segments.Last() + " " + image.Title);
                         using (var wc = new WebClient())
@@ -94,16 +95,15 @@ namespace HTTP
 
         }
 
-        private static async Task<List<string>> GetChildPages(string sourcePage)
+        private static async Task<List<Markup>> GetChildPages(string sourcePage, IEnumerable<string> storedUrls)
         {
-            var pages = new List<string>();
-            var links = GetLinks(sourcePage);
+			var pages = new List<Markup>();
+			var links = GetLinks(sourcePage, storedUrls);
             foreach (var link in links)
             {
                 try
                 {
-                    var item = await GetHtml(link);
-                    pages.Add(item);
+					pages.Add(new Markup { Url = link, Html = await GetHtml(link) });
                 }
                 catch (Exception e)
                 {
@@ -113,46 +113,54 @@ namespace HTTP
             return pages;
         }
 
-        private static List<string> GetLinks(string mainPage)
+        private static List<string> GetLinks(string mainPage, IEnumerable<string> storedUrls)
         {
             var linksArray = new List<string>();
-
 
             var doc = new HtmlDocument();
             doc.LoadHtml(mainPage);
             var nodes = doc.DocumentNode.SelectNodes("//a");
             foreach (var link in nodes
-                .Select(node => node.Attributes["href"].Value)
-                .Where(link => !string.IsNullOrEmpty(link) && !linksArray.Contains(link)))
+	            .Select(node => node.Attributes["href"].Value)
+	            .Where(link => !string.IsNullOrEmpty(link) && !linksArray.Contains(link))
+				.Where(link => !storedUrls.Any(x => x.Equals(link))))
             {
-                linksArray.Add(link);
+	            linksArray.Add(link);
             }
             return linksArray;
         }
 
-        private static IEnumerable<Img> Extras(string url, HtmlDocument doc)
-        {
-          //  var reu = new List<Img>();
-            var htmlNodes = doc.DocumentNode.Descendants("img");
-            foreach (var img in htmlNodes)
-            {
-                var t = img.ParentNode.Attributes["title"];
-                var s = img.Attributes["src"].Value;
-                if (s.StartsWith("/") || !s.StartsWith("http")) s = string.Format("{0}{1}", url, s);
-                    yield return new Img { Url = new Uri(s), Title = (t != null ? t.Value : "") };
-               // reu.Add(new Img { Url = new Uri(s), Title = (t != null ? t.Value : "") });
-            }
-           // return reu;
-        }
+	    private static IEnumerable<Img> Extras(string url, HtmlDocument doc, List<FileExtension> extensions)
+	    {
+		    var htmlNodes = doc.DocumentNode.Descendants("img");
+		    foreach (var img in htmlNodes)
+		    {
+			    var title = img.ParentNode.Attributes["title"];
+			    var source = img.Attributes["src"] != null ? img.Attributes["src"].Value : string.Empty;
+				if (extensions.Any(e => IsApprovedExtension(source, e)))
+			    {
+				    if (source.StartsWith("/") || !source.StartsWith("http")) source = string.Format("{0}{1}", url, source);
+				    yield return new Img {Url = new Uri(source), Title = (title != null ? title.Value : "")};
+			    }
+		    }
+	    }
 
-        private static async Task<string> GetHtml(string requestUri)
+	    private static bool IsApprovedExtension(string source, FileExtension approvedExtension)
+	    {
+		    var sourceExtension = Path.GetExtension(source);
+		    if (sourceExtension == null) return false;
+		    var upper = sourceExtension.Remove(0, sourceExtension.Length > 0 ? 1 : 0).ToUpper();
+		    return approvedExtension.ToString().ToUpper().Equals(upper);
+	    }
+
+	    private static async Task<string> GetHtml(string requestUri)
         {
             string result;
-            using (HttpClient client = new HttpClient())
+            using (var client = new HttpClient())
             {
-                using (HttpResponseMessage responseMessage = await client.GetAsync(requestUri))
+                using (var responseMessage = await client.GetAsync(requestUri))
                 {
-                    using (HttpContent content = responseMessage.Content)
+                    using (var content = responseMessage.Content)
                     {
                         result = await content.ReadAsStringAsync();
                     }
@@ -162,19 +170,26 @@ namespace HTTP
         }
     }
 
-    public enum DomainLimitation
+	public class Markup
+	{
+		public string Url { get; set; }
+		public string Html { get; set; }
+	}
+
+	public enum DomainLimitation
     {
         Without,
         CurrentDomain,
         SourceURL
     }
 
-    public enum FileExtention
+    public enum FileExtension
     {
         GIF,
         JPEG,
         JPG,
-        PDF
+        PDF,
+	    PNG
     }
 
     public class Get
